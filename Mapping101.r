67fac703b8ded3b21b8bd2164f252034c7cf28b1
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-28 20:52:53 +0100

    #001

--- Mapping101.r
+++ Mapping101.r
@@ -13,3 +13,13 @@ library(ggplot2) # tidyverse data visualization package
 
 nz_elev = rast(system.file("raster/nz_elev.tif", package = "spDataLarge"))
 
+# Add fill layer to nz shape
+tm_shape(nz) +
+  tm_fill() 
+# Add border layer to nz shape
+tm_shape(nz) +
+  tm_borders() 
+# Add fill and border layers to nz shape
+tm_shape(nz) +
+  tm_fill() +
+  tm_borders() 
