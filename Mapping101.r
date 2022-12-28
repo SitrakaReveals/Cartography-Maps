a2728c06a015d7013071f8fd0d2cbc157a7148f7
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-28 20:52:27 +0100

    #comments

--- Mapping101.r
+++ Mapping101.r
@@ -1,7 +1,15 @@
-"" https://geocompr.robinlovelace.net/adv-map.html 
+#https://geocompr.robinlovelace.net/adv-map.html 
 
 library(sf)
 library(terra)
 library(dplyr)
 library(spData)
 library(spDataLarge)
+
+
+library(tmap)    # for static and interactive maps
+library(leaflet) # for interactive maps
+library(ggplot2) # tidyverse data visualization package
+
+nz_elev = rast(system.file("raster/nz_elev.tif", package = "spDataLarge"))
+
