b281cf4318b38d1632e0135651f9fbd9f1f3f10f
Sitraka Forler <49253764+Sitraka17@users.noreply.github.com>
2022-12-30 20:10:37 +0100

    hjh

--- LUXinR.r
+++ LUXinR.r
@@ -7,3 +7,16 @@ library(rnaturalearth)
 # Next, use the ne_countries function from the rnaturalearth package to
 # download a shapefile of Luxembourg
 luxembourg <- ne_countries(scale = "medium", country = "Luxembourg", returnclass = "sf")
+
+# Use the st_geometry function from the sf package to extract the geometry
+# of Luxembourg as a simple feature object
+luxembourg_geom <- st_geometry(luxembourg)
+
+# Now, create a ggplot object and add the Luxembourg geometry to it
+# using the geom_sf function. You can also customize the appearance of
+# the map by setting additional aesthetic mappings, such as fill color
+# or line color.
+plot <- ggplot() + geom_sf(data = luxembourg, fill = "gray")
+
+# Finally, use the ggplot function to display the plot
+ggplot(plot)
